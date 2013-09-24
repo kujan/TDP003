111b2041a284283d459f2a2529f3e7730f5151c6
kujan <fredrik.k91>
2013-09-24 11:49:42 +0200

    Ny data.json och data_test för python3

--- data_test.py
+++ data_test.py
@@ -6,12 +6,13 @@ import unittest
 import data
 import hashlib
 import sys
+
 md5 = hashlib.md5
 class DataTest(unittest.TestCase):
     def setUp(self):
-        self.expected_data = [{u'big_image': u'XXX', u'project_name': u'python data-module test script', u'course_name': u'OK\xc4NT', u'group_size': 2, u'end_date': u'2009-09-06', u'techniques_used': [u'python'], u'academic_credits': u'WUT?', u'small_image': u'X', u'long_description': u'no no no', u'course_id': u'TDP003', u'project_no': 1, u'external_link': u'YY', u'short_description': u'no', u'start_date': u'2009-09-05', u'lulz_had': u'many'}, {u'big_image': u'XXX', u'project_name': u'NEJ', u'course_name': u'OK\xc4NT', u'group_size': 4, u'end_date': u'2009-09-08', u'techniques_used': [u'c++', u'csv', u'python'], u'academic_credits': u'WUT?', u'small_image': u'X', u'long_description': u'no no no', u'course_id': u'TDP003', u'project_no': 3, u'external_link': u'YY', u'short_description': u'no', u'start_date': u'2009-09-07', u'lulz_had': u'few'}, {u'big_image': u'XXX', u'project_name': u'2007', u'course_name': u'OK\xc4NT', u'group_size': 6, u'end_date': u'2009-09-09', u'techniques_used': [u'ada', u'python'], u'academic_credits': u'WUT?', u'small_image': u'X', u'long_description': u'no no no', u'course_id': u'TDP003', u'project_no': 2, u'external_link': u'YY', u'short_description': u'no', u'start_date': u'2009-09-08', u'lulz_had': u'medium'}, {u'big_image': u'XXX', u'project_name': u',', u'course_name': u'HOHO', u'group_size': 8, u'end_date': u'2009-09-07', u'techniques_used': [], u'academic_credits': u'WUT?', u'small_image': u'X', u'long_description': u'no no no', u'course_id': u' "', u'project_no': 4, u'external_link': u'YY', u'short_description': u'no', u'start_date': u'2009-09-06', u'lulz_had': u'over 9000'}]
-        self.expected_technique_data = [u'ada', u'c++', u'csv', u'python']
-        self.expected_technique_stat_data = {u'python': [{u'id': 2, u'name': u'2007'}, {u'id': 3, u'name': u'NEJ'}, {u'id': 1, u'name': u'python data-module test script'}], u'csv': [{u'id': 3, u'name': u'NEJ'}], u'c++': [{u'id': 3, u'name': u'NEJ'}], u'ada': [{u'id': 2, u'name': u'2007'}]}
+        self.expected_data = [{'big_image': 'XXX', 'project_name': 'python data-module test script', 'course_name': 'OK\xc4NT', 'group_size': 2, 'end_date': '2009-09-06', 'techniques_used': ['python'], 'academic_credits': 'WUT?', 'small_image': 'X', 'long_description': 'no no no', 'course_id': 'TDP003', 'project_no': 1, 'external_link': 'YY', 'short_description': 'no', 'start_date': '2009-09-05', 'lulz_had': 'many'}, {'big_image': 'XXX', 'project_name': 'NEJ', 'course_name': 'OK\xc4NT', 'group_size': 4, 'end_date': '2009-09-08', 'techniques_used': ['c++', 'csv', 'python'], 'academic_credits': 'WUT?', 'small_image': 'X', 'long_description': 'no no no', 'course_id': 'TDP003', 'project_no': 3, 'external_link': 'YY', 'short_description': 'no', 'start_date': '2009-09-07', 'lulz_had': 'few'}, {'big_image': 'XXX', 'project_name': '2007', 'course_name': 'OK\xc4NT', 'group_size': 6, 'end_date': '2009-09-09', 'techniques_used': ['ada', 'python'], 'academic_credits': 'WUT?', 'small_image': 'X', 'long_description': 'no no no', 'course_id': 'TDP003', 'project_no': 2, 'external_link': 'YY', 'short_description': 'no', 'start_date': '2009-09-08', 'lulz_had': 'medium'}, {'big_image': 'XXX', 'project_name': ',', 'course_name': 'HOHO', 'group_size': 8, 'end_date': '2009-09-07', 'techniques_used': [], 'academic_credits': 'WUT?', 'small_image': 'X', 'long_description': 'no no no', 'course_id': ' "', 'project_no': 4, 'external_link': 'YY', 'short_description': 'no', 'start_date': '2009-09-06', 'lulz_had': 'over 9000'}]
+        self.expected_technique_data = ['ada', 'c++', 'csv', 'python']
+        self.expected_technique_stat_data = {'python': [{'id': 2, 'name': '2007'}, {'id': 3, 'name': 'NEJ'}, {'id': 1, 'name': 'python data-module test script'}], 'csv': [{'id': 3, 'name': 'NEJ'}], 'c++': [{'id': 3, 'name': 'NEJ'}], 'ada': [{'id': 2, 'name': '2007'}]}
 
         self.loaded_data = data.load("data.json")
 
@@ -23,21 +24,21 @@ class DataTest(unittest.TestCase):
         self.assertEqual(data.get_project_count(self.loaded_data), 4)
         
     def test_get_project(self):
-        self.assertEqual(data.get_project(self.loaded_data, 1)[u'project_no'], 1)
-        self.assertEqual(data.get_project(self.loaded_data, 2)[u'project_no'], 2)
-        self.assertEqual(data.get_project(self.loaded_data, 3)[u'project_no'], 3)
-        self.assertEqual(data.get_project(self.loaded_data, 4)[u'project_no'], 4)
+        self.assertEqual(data.get_project(self.loaded_data, 1)['project_no'], 1)
+        self.assertEqual(data.get_project(self.loaded_data, 2)['project_no'], 2)
+        self.assertEqual(data.get_project(self.loaded_data, 3)['project_no'], 3)
+        self.assertEqual(data.get_project(self.loaded_data, 4)['project_no'], 4)
         self.assertEqual(data.get_project(self.loaded_data, 42), None)
 
     def test_search(self):
         self.assertEqual(len(data.search(self.loaded_data)), 4)
 
-        self.assertEqual(len(data.search(self.loaded_data, techniques=[u'csv'])), 1)
+        self.assertEqual(len(data.search(self.loaded_data, techniques=['csv'])), 1)
 
         res = data.search(self.loaded_data, sort_order='asc',techniques=["python"])
-        self.assertEqual(res[0][u'project_no'], 1)
-        self.assertEqual(res[1][u'project_no'], 3)
-        self.assertEqual(res[2][u'project_no'], 2)
+        self.assertEqual(res[0]['project_no'], 1)
+        self.assertEqual(res[1]['project_no'], 3)
+        self.assertEqual(res[2]['project_no'], 2)
 
 
         res = data.search(self.loaded_data, 
@@ -45,9 +46,9 @@ class DataTest(unittest.TestCase):
                                      search='okänt', 
                                      search_fields=['project_no','project_name','course_name'])
         self.assertEqual(len(res), 3)
-        self.assertEqual(res[0][u'project_no'], 2)
-        self.assertEqual(res[1][u'project_no'], 3)
-        self.assertEqual(res[2][u'project_no'], 1)
+        self.assertEqual(res[0]['project_no'], 2)
+        self.assertEqual(res[1]['project_no'], 3)
+        self.assertEqual(res[2]['project_no'], 1)
         
         res = data.search(self.loaded_data, 
                                      search="okänt", 
@@ -64,10 +65,10 @@ class DataTest(unittest.TestCase):
         self.assertEqual(len(res), 0)
 
         res = data.search(self.loaded_data, sort_by='group_size')
-        self.assertEqual(res[0][u'project_no'], 4)
-        self.assertEqual(res[1][u'project_no'], 2)
-        self.assertEqual(res[2][u'project_no'], 3)
-        self.assertEqual(res[3][u'project_no'], 1)
+        self.assertEqual(res[0]['project_no'], 4)
+        self.assertEqual(res[1]['project_no'], 2)
+        self.assertEqual(res[2]['project_no'], 3)
+        self.assertEqual(res[3]['project_no'], 1)
 
     def test_get_techniques(self):
         res = data.get_techniques(self.loaded_data)
@@ -79,7 +80,9 @@ class DataTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    #print "Test:     ", md5.new(sys.argv[0]).hexdigest()
+    #print "Test data:", md5.new("data.json").hexdigest()
     print ("Test:     ", md5(sys.argv[0].encode('UTF-8')).hexdigest())
     print ("Test data:", md5(b"data.json").hexdigest())
-    print
+    print()
     unittest.main()
